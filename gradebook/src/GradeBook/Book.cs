889fa2d4b7d255dbb18a1c010724328f5382e3b8
Rey Perez <ichibanrei@gmail.com>
2020-03-10 15:12:13 -0700

    Lesson 6 starting
    initial commit up to lesson 6

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Collections.Generic;
+namespace GradeBook
+{
+    public class Book
+    {
+        List<double> grades;
+        private string name;
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+
+            grades.Add(grade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.Low = double.MaxValue;
+            result.High = double.MinValue;
+            
+            foreach(double grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+            return result;
+        }
+    }
+}
---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Rey's GradeBook");
+            book.AddGrade(12.7);
+            book.AddGrade(10.3);
+            book.AddGrade(6.11);
+            book.AddGrade(4.11);
+            book.AddGrade(25.1);            
+            book.AddGrade(56.1);
+
+            var stats = book.GetStatistics();
+            
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"the highest grade is {stats.High:N1}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+        }
+    }
+}
